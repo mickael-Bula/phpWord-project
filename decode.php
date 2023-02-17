47e5b8c3cca54ad8e855009fa1066ba24beca677
Locullus <bula.mickael@gmail.com>
2023-02-17 22:07:18 +0100

    utilisation des tags classiques
    les doubles accolades ne fonctionnent pas, même après ajout de la méthode setMacroChars()

--- decode.php
+++ decode.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $tags = [
     "lastname" => "Bula",
     "firstname" => "mika",
+    "name" => "Mickaël Bula",
     "age" => "51"
 ];
 
@@ -18,11 +19,18 @@ $pathToSave = __DIR__ . '/var/result.docx';
 try {
     $templateProcessor = new TemplateProcessor($source);
 
+    // même en ajoutant les méthodes manquantes dans la classe TemplateProcessor, je n'obtiens l'effet attendu : les doubles accolades ne sont pas reconnues comme tags.
     $templateProcessor->setMacroChars('{{', '}}');
 
+    // affectation des variables à l'aide d'un tableau
     $templateProcessor->setValues($tags);
 
+    // ajout d'une variable supplémentaire
+    $templateProcessor->setValue('date', date("d-m-Y"));
+
     $templateProcessor->saveAs($pathToSave);
+
+    echo "le fichier a été copié";
 
 } catch ( CopyFileException | CreateTemporaryFileException $e ) {
     echo "problème avec le fichier .docx" . $e->getMessage();
